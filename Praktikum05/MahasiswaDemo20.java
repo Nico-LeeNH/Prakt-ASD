cbced1ea89141aa8afd75e4174c868c1bf49bad8
Nico Lee <new.nikolee2007@gmail.com>
2026-04-04 19:55:33 +0700

    commit

---
+++ Praktikum05/MahasiswaDemo20.java
@@ -0,0 +1,49 @@
+package Praktikum05;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        MahasiswaBerprestasi20 list = new MahasiswaBerprestasi20(n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK   : ");
+            double ipk = Double.parseDouble(sc.nextLine());
+
+            Mahasiswa20 m = new Mahasiswa20(nim, nama, kelas, ipk);
+            list.tambah(m);
+
+            System.out.println("---------------------------------");
+        }
+
+        System.out.println("Data sebelum sorting:");
+        list.tampil();
+
+        System.out.println("Data setelah sorting IPK (DESC)");
+        list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        list.selectionSort();
+        list.tampil();
+
+        sc.close();
+    }
+}
